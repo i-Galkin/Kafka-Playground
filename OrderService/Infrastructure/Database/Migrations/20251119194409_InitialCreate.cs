bca0a4669f8b7d0348171b45d402eb655286e5ef
i-Galkin <beekwar@gmail.com>
2025-11-20 14:54:56 +0100

    Add migrations

---
+++ OrderService/Infrastructure/Database/Migrations/20251119194409_InitialCreate.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace OrderService.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "failed_order_messages",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    topic = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    partition = table.Column<int>(type: "integer", nullable: false),
+                    offset = table.Column<long>(type: "bigint", nullable: false),
+                    key = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    value = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    error_message = table.Column<string>(type: "character varying(2500)", maxLength: 2500, nullable: true),
+                    stack_trace = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    retry_count = table.Column<int>(type: "integer", nullable: false),
+                    failed_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    consumer_name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_failed_order_messages", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    order_id = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    customer_id = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    processed_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    partition = table.Column<int>(type: "integer", nullable: false),
+                    offset = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orders", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "idx_failed_orders_messages_failed_at",
+                table: "failed_order_messages",
+                column: "failed_at");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_failed_orders_messages_topic_partition_offset",
+                table: "failed_order_messages",
+                columns: new[] { "topic", "partition", "offset" });
+
+            migrationBuilder.CreateIndex(
+                name: "idx_orders_created_at",
+                table: "orders",
+                column: "created_at");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_orders_customer_id",
+                table: "orders",
+                column: "customer_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "failed_order_messages");
+
+            migrationBuilder.DropTable(
+                name: "orders");
+        }
+    }
+}
---
+++ OrderService/Infrastructure/Database/Migrations/20251120133607_ExtendFieldsLength.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendFieldsLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "stack_trace",
+                table: "failed_order_messages",
+                type: "text",
+                maxLength: 2147483647,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "error_message",
+                table: "failed_order_messages",
+                type: "text",
+                maxLength: 2147483647,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(2500)",
+                oldMaxLength: 2500,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "stack_trace",
+                table: "failed_order_messages",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldMaxLength: 2147483647,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "error_message",
+                table: "failed_order_messages",
+                type: "character varying(2500)",
+                maxLength: 2500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldMaxLength: 2147483647,
+                oldNullable: true);
+        }
+    }
+}
